c1b6f2d8d63a171e32d9ba681b7ae2522ce04ad9
Arthur Poulet <arthur.poulet@cryptolab.net>
2016-05-15 15:54:25 +0200

    add send_login, ping, pong

--- src/CrystalIrc/client/connect.cr
+++ src/CrystalIrc/client/connect.cr
@@ -2,17 +2,19 @@ module CrystalIrc
   class Client
 
     module Connect
-      def connect(nick : String, ip : String, port : UInt16, ssl : Bool, user : String?, ident : String?, domain : String?, irc_server : String?) : TCPSocket?
+      # domain and irc_server are not used
+      def connect(nick : String, ip : String, port : UInt16, ssl : Bool, user : String?, realname : String?, pass : String?, domain : String?, irc_server : String?) : TCPSocket?
         raise NotImplemented.new "SSL is not ready yet" if ssl
         socket = TCPSocket.new ip, port
-        user ||= nick
-        ident ||= nick
-        domain ||= "localhost"
-        irc_server ||= "irc_server"
-        socket.puts "NICK #{nick}"
-        socket.puts "USER #{user} \"#{domain}\" \"#{irc_server}\" :#{ident}"
+        send_login(socket: socket, nick: nick, user: user || nick, realname: realname || nick, pass: pass)
         # handle answer
         socket
+      end
+
+      def send_login(socket : TCPSocket, nick : String, user : String, realname : String?, pass : String?, domain = "0", irc_server = "*")
+        socket.puts "PASS #{pass}" if pass
+        socket.puts "NICK #{nick}"
+        socket.puts "USER #{user} \"#{domain}\" \"#{irc_server}\" :#{realname}"
       end
     end
 
--- src/CrystalIrc/client/ping.cr
+++ src/CrystalIrc/client/ping.cr
@@ -2,7 +2,13 @@ module CrystalIrc
   class Client
 
     module Ping
+      def ping(socket : TCPSocket, message = "0")
+        socket.puts "PING #{message}"
+      end
 
+      def pong(socket : TCPSocket, message = "0")
+        socket.puts "PONG #{message}"
+      end
     end
 
   end
