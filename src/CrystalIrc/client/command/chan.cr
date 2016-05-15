1d420b0d818b042e9107f60765f59a59fd117958
Arthur Poulet <arthur.poulet@cryptolab.net>
2016-05-15 23:27:32 +0200

    improve chan commands
    - format_list replace format_chan
    - remove empty lines

--- src/CrystalIrc/client/command/chan.cr
+++ src/CrystalIrc/client/command/chan.cr
@@ -3,8 +3,8 @@ module CrystalIrc
     module Command
 
       module Chan
-        # Formats the chans to join: #chan1,#chan2
-        def format_chans(chans : Array(CrystalIrc::Chan))
+        # Formats the chans to join: #chan1,#chan2 (works with users by the same way)
+        def format_list(chans : Array(CrystalIrc::Chan | CrystalIrc::User))
           chans.map do |c|
             c.name
           end.uniq.join(",")
@@ -13,16 +13,14 @@ module CrystalIrc
         # Requests to join the chan(s) chans, with password(s) passwords.
         # The passwords may be ignored if not needed.
         def join(chans : Array(CrystalIrc::Chan), passwords : Array(String) = [""])
-          to_join = format_chans(chans)
-
+          to_join = format_list(chans)
           passes = passwords.uniq.join(",")
-
           send_raw "JOIN #{to_join} #{passes}"
         end
 
         # Requests to leave the channel(s) chans, with an optional part message msg.
         def part(chans : Array(CrystalIrc::Chan), msg : String?)
-          to_leave = format_chans(chans)
+          to_leave = format_list(chans)
           msg = ":#{msg}" if msg
           send_raw "PART #{to_leave} #{msg}"
         end
@@ -45,14 +43,14 @@ module CrystalIrc
         # If no channel is given, requests the names of the users in every
         # known channel.
         def names(chans : Array(CrystalIrc::Chan)?)
-          target = chans ? format_chans(chans) : ""
+          target = chans ? format_list(chans) : ""
           send_raw "NAMES #{target}"
         end
 
         # Lists the channels and their topics.
         # If the chans parameter is given, lists the status of the given chans.
         def list(chans : Array(CrystalIrc::Chan)?)
-          target = chans ? format_chans(chans) : ""
+          target = chans ? format_list(chans) : ""
           send_raw "LIST #{target}"
         end
 
@@ -64,12 +62,8 @@ module CrystalIrc
         # Kicks the user(s) users from the channel(s) chans.
         # The reason of the kick will be displayed if given as a parameter.
         def kick(chans : Array(CrystalIrc::Chan), users : Array(CrystalIrc::User), reason : String?)
-          chan = format_chans(chans)
-
-          targets = users.map do |u|
-            u.name
-          end.uniq.join(",")
-
+          chan = format_list(chans)
+          targets = format_list(users)
           reason = ":#{reason}" if reason
           send_raw "KICK #{chan} #{targets} #{reason}"
         end
