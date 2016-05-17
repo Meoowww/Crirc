4fbf8c8d5203e2bec1586af380f757f5895bf3f7
Arthur Poulet <arthur.poulet@cryptolab.net>
2016-05-17 15:20:02 +0200

    rfc ? We don't give a f

--- spec/CrystalIrc/user.cr
+++ spec/CrystalIrc/user.cr
@@ -3,6 +3,9 @@ describe CrystalIrc::User do
   it "instanciation" do
     e = CrystalIrc::User.new "test-works"
     e.name.should eq("test-works")
+    # RFC ? ahahah get out the way
+    e = CrystalIrc::User.new "test_works"
+    e.name.should eq("test_works")
     e = CrystalIrc::User.new "#test-works"
     e.name.should eq("#test-works")
     e = CrystalIrc::User.new("a"*50)
@@ -11,7 +14,6 @@ describe CrystalIrc::User do
     expect_raises(CrystalIrc::InvalidUserName) { CrystalIrc::User.new("1a") }
     expect_raises(CrystalIrc::InvalidUserName) { CrystalIrc::User.new("a"*51) }
     expect_raises(CrystalIrc::InvalidUserName) { CrystalIrc::User.new("spaces in the name") }
-    expect_raises(CrystalIrc::InvalidUserName) { CrystalIrc::User.new("underscores_in_the_name") }
   end
 
 
--- src/CrystalIrc/user.cr
+++ src/CrystalIrc/user.cr
@@ -8,7 +8,7 @@ module CrystalIrc
 
     def initialize(@name)
       raise InvalidUserName.new "The User name must not be empty" if @name.empty?
-      raise InvalidUserName.new "The User name must contains at most 63 valid characters" if !@name.match(/\A(?!.{51,})(\#?([a-zA-Z])([a-zA-Z0-9\-\[\]\\\`\^\{\}]+))\Z/)
+      raise InvalidUserName.new "The User name must contains at most 63 valid characters" if !@name.match(/\A(?!.{51,})(\#?([a-zA-Z])([a-zA-Z0-9_\-\[\]\\\`\^\{\}]+))\Z/)
     end
 
   end
