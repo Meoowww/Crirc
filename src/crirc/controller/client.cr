7020146105ddc17493dcafe81e60e8073d88ad7f
Arthur Poulet <arthur.poulet@sceptique.eu>
2017-11-03 00:21:04 +0100

    Use controller puts instead of network puts

--- src/crirc/controller/client.cr
+++ src/crirc/controller/client.cr
@@ -27,9 +27,9 @@ class Crirc::Controller::Client
   end
 
   def init
-    @network.puts "PASS #{@network.pass}" if @network.pass
-    @network.puts "NICK #{@network.nick.to_s}"
-    @network.puts "USER #{@network.user.to_s} \"#{@network.domain}\" \"#{@network.irc_server}\" :#{@network.realname.to_s}"
+    puts "PASS #{@network.pass}" if @network.pass
+    puts "NICK #{@network.nick.to_s}"
+    puts "USER #{@network.user.to_s} \"#{@network.domain}\" \"#{@network.irc_server}\" :#{@network.realname.to_s}"
   end
 
   def on_ready(&b) : Client
